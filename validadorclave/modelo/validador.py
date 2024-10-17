36fa88d9ad29f78005949a487aec57bc15721788
Miguel Guarnizo <miguelangelguarnizosalazar@gmail.com>
2024-10-17 11:26:08 -0500

    class creation

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,16 @@
 # TODO: Implementa el código del ejercicio aquí
+
+from abc import ABC, abstractmethod
+from validadorclave.modelo.errores import *
+
+class ReglaValidacion:
+    pass
+
+class Validador:
+    pass
+
+class ReglaValidacionGanimedes(ReglaValidacion):
+    pass
+
+class ReglaValidacionCalisto(ReglaValidacion):
+    pass
