d8f4a7a33655ce53c74149a561ff82e027f1eb72
Miguel Guarnizo <miguelangelguarnizosalazar@gmail.com>
2024-10-17 11:28:35 -0500

    first method of the class one

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -4,8 +4,13 @@ from abc import ABC, abstractmethod
 from validadorclave.modelo.errores import *
 
 class ReglaValidacion:
-    def es_valida(self, clave):
+    def __init__(self, longitud_esperada: int):
+        self._longitud_esperada: int = longitud_esperada
+
+    @abstractmethod
+    def es_valida(self, clave: str) -> bool:
         pass
+
 
 class Validador:
     def __init__(self, regla: ReglaValidacion):
